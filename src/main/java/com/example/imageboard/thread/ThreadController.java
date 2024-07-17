1657a2d4ef9d856ca924f863bfce41d96063fc37
martincorradetti <martin.corradetti@hotmail.com>
2024-07-17 03:01:12 +0200

    Commit and psuh

--- src/main/java/com/example/imageboard/thread/ThreadController.java
+++ src/main/java/com/example/imageboard/thread/ThreadController.java
@@ -1,5 +1,6 @@
 package com.example.imageboard.thread;
 
+import com.example.imageboard.thread.ThreadService;
 import com.example.imageboard.thread.dto.ThreadDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +9,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/threads")
+@RequestMapping("/api/forums/{forumId}/threads")
 public class ThreadController {
 
     private final ThreadService threadService;
@@ -17,47 +18,39 @@ public class ThreadController {
         this.threadService = threadService;
     }
 
-    // Endpoint to create a thread
-    @PostMapping("forum/{forumId}")
+    // Endpoint to create a thread within a specific forum
+    @PostMapping
     public ResponseEntity<ThreadDto> create(@PathVariable Long forumId, @RequestBody ThreadDto threadDto) {
         ThreadDto createdThread = threadService.create(forumId, threadDto);
-        return ResponseEntity.created(URI.create("/api/threads/" + createdThread.getId()))
+        return ResponseEntity.created(URI.create("/api/forums/" + forumId + "/threads/" + createdThread.getId()))
                 .body(createdThread);
     }
 
     // Endpoint to update a thread within a specific forum
-    @PutMapping("/{forumId}/{threadId}")
-    public ResponseEntity<ThreadDto> update(@PathVariable Long forumId, @PathVariable Long threadId, @RequestBody ThreadDto threadDto){
+    @PutMapping("/{threadId}")
+    public ResponseEntity<ThreadDto> update(@PathVariable Long forumId, @PathVariable Long threadId, @RequestBody ThreadDto threadDto) {
         ThreadDto updatedThread = threadService.update(forumId, threadId, threadDto);
         return ResponseEntity.ok(updatedThread);
     }
 
     // Endpoint to delete a thread within a specific forum
-    @DeleteMapping("/{forumId}/{threadId}")
+    @DeleteMapping("/{threadId}")
     public ResponseEntity<Void> delete(@PathVariable Long forumId, @PathVariable Long threadId) {
         threadService.delete(forumId, threadId);
         return ResponseEntity.noContent().build();
     }
 
-    // Endpoint to get all threads
+    // Endpoint to get all threads within a specific forum
     @GetMapping
-    public ResponseEntity<List<ThreadDto>> getAll() {
-        List<ThreadDto> threads = threadService.getAll();
-        return ResponseEntity.ok(threads);
-    }
-
-    // Endpoint to get a thread by ID
-    @GetMapping("/{id}")
-    public ResponseEntity<ThreadDto> getById(@PathVariable Long id) {
-        ThreadDto threadDto = threadService.getById(id);
-        return ResponseEntity.ok(threadDto);
-    }
-
-    // Endpoint to get all threads by forum ID
-    @GetMapping("/forum/{forumId}")
-    public ResponseEntity<List<ThreadDto>> getAllByForumId(@PathVariable Long forumId) {
+    public ResponseEntity<List<ThreadDto>> getAll(@PathVariable Long forumId) {
         List<ThreadDto> threads = threadService.getByForumId(forumId);
         return ResponseEntity.ok(threads);
     }
-}
 
+    // Endpoint to get a thread by ID within a specific forum
+    @GetMapping("/{threadId}")
+    public ResponseEntity<ThreadDto> getById(@PathVariable Long forumId, @PathVariable Long threadId) {
+        ThreadDto threadDto = threadService.getById(threadId);
+        return ResponseEntity.ok(threadDto);
+    }
+}
